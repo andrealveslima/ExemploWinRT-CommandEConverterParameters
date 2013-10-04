925d05f986914e8b419d33678051caeb30fc5fd0
andrealveslima <andrealveslimeira@gmail.com>
2013-10-04 12:46:11 +0200

    Adicionando arquivos faltando

---
+++ CommandEConverterParameters/CommandEConverterParameters/MostrarMensagemCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandEConverterParameters
+{
+    public class MostrarMensagemCommand : System.Windows.Input.ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            MostrarMensagemParameters mostrarMensagemParameters = parameter as MostrarMensagemParameters;
+
+            if (mostrarMensagemParameters != null)
+                await new Windows.UI.Popups.MessageDialog(mostrarMensagemParameters.Mensagem, mostrarMensagemParameters.Titulo).ShowAsync();
+        }
+    }
+}
---
+++ CommandEConverterParameters/CommandEConverterParameters/MostrarMensagemParameters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandEConverterParameters
+{
+    public class MostrarMensagemParameters : Windows.UI.Xaml.DependencyObject
+    {
+        //public string Titulo { get; set; }
+        //public string Mensagem { get; set; }
+
+        public static readonly Windows.UI.Xaml.DependencyProperty TituloProperty =
+            Windows.UI.Xaml.DependencyProperty.Register(
+            "Titulo", typeof(string), typeof(MostrarMensagemParameters), null);
+
+        public string Titulo
+        {
+            get { return (string)this.GetValue(TituloProperty); }
+            set { this.SetValue(TituloProperty, value); }
+        }
+
+        public static readonly Windows.UI.Xaml.DependencyProperty MensagemProperty =
+            Windows.UI.Xaml.DependencyProperty.Register(
+            "Mensagem", typeof(string), typeof(MostrarMensagemParameters), null);
+
+        public string Mensagem
+        {
+            get { return (string)this.GetValue(MensagemProperty); }
+            set { this.SetValue(MensagemProperty, value); }
+        }
+    }
+}
